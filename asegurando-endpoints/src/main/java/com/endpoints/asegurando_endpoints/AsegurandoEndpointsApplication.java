89657a793f61aa2b9aedce7dba047e80596e6a41
Timms <timmsarizafredy2@gmail.com>
2026-09-15 15:36:39 -0500

    Base creada

---
+++ asegurando-endpoints/src/main/java/com/endpoints/asegurando_endpoints/AsegurandoEndpointsApplication.java
@@ -0,0 +1,13 @@
+package com.endpoints.asegurando_endpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsegurandoEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsegurandoEndpointsApplication.class, args);
+	}
+
+}
---
+++ asegurando-endpoints/src/test/java/com/endpoints/asegurando_endpoints/AsegurandoEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endpoints.asegurando_endpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsegurandoEndpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
